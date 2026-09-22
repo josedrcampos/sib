99ffc706307edfed09c6d5fa7f1a7cf06b16b78f
josedrcampos <josedrdcampos@gmail.com>
2026-09-22 22:35:09 +0100

    name change

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "Diogo Cachetas"
-__credits__ = ["Diogo Cachetas", "João Capela", "João Correia", "Fernando Cruz", "Vítor Pereira"]
+__author__ = "José Campos"
+__credits__ = ["José Campos", "João Capela", "Fernando Cruz", "Vítor Pereira", "João Correia"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "Diogo Cachetas"
-__email__ = "diogo.cachetas@inl.int"
+__maintainer__ = "José Campos"
+__email__ = "pg54900@alunos.uminho.pt"
 
 
 
